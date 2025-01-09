9b56cba80bcc843030cbaa288e03ab452881977e
jessealvessilva <67879529+jessealvessilva@users.noreply.github.com>
2025-01-09 11:52:16 -0300

    Teste

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:whatsappcentral/view/contact_list.dart';
 import 'view/logins_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyApp()); //
 }
 
 class MyApp extends StatelessWidget {
