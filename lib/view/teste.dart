28f3f95e3a11e20f042b61c79c13e22a10757dab
jessealvessilva <67879529+jessealvessilva@users.noreply.github.com>
2025-02-11 09:02:59 -0300

    Fonte de Teste

--- lib/view/teste.dart
+++ lib/view/teste.dart
@@ -16,12 +16,41 @@ class _MyWidgetState extends State<MyWidget> {
 
   @override
   void initState() {
-    // lista = _list();
     super.initState();
   }
 
+  Future<String> getFutureDados() async =>
+      await Future.delayed(Duration(seconds: 20), () {
+        return "Dados recebidos...";
+      });
+
   @override
   Widget build(BuildContext context) {
-    return const Text("");
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        title: Text(
+          'Future Builder',
+        ),
+      ),
+      body: Container(
+        child: FutureBuilder(
+            future: getFutureDados(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Center(
+                  child: Text(
+                    snapshot.data!,
+                    style: const TextStyle(fontSize: 20.0),
+                  ),
+                );
+              } else {
+                return Center(
+                  child: const CircularProgressIndicator(),
+                );
+              }
+            }),
+      ),
+    );
   }
 }
